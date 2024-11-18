2d7763918bd4e1267802752ddbf2628c0ff0459f
이지민 <jmlee@emro.co.kr>
2024-11-18 13:42:51 +0900

    chore : css fix for navbar

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -6,8 +6,8 @@ const HomeHeader = ({ onFire }) => {
   return (
     <header className="App-header">
       <Firework />
-      <h1 className="Title">Welcome to my Homepage</h1>
-      업데이트 준비중 ~.~ 기대해주세요~ㅎㅎ<p className="tip">사진을 눌러보세요</p>
+      <h1 className="Title">Ji Min Lee</h1>
+      Welcome to my homepage!<p className="tip">사진을 눌러보세요</p>
     </header>
   );
 };
